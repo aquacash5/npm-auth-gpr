db184441dc109f149312e8917459c9cd1e00c52b
Kyle Bloom <kyle@symless.com>
2020-09-16 08:19:07 +0100

    Add test

---
+++ test.js
@@ -0,0 +1,17 @@
+const { promisify } = require(`util`);
+const readFile = promisify(require(`fs`).readFile);
+const core = require(`@actions/core`);
+
+const run = async () => {
+  const token = core.getInput(`token`);
+
+  try {
+    const fileContent = await readFile(`.npmrc`);
+    if (!fileContent.endsWith(token))
+      throw new Error(`Token not found in .npmrc file`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
